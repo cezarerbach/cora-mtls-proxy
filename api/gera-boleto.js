2503b90269d07e4bf0fc74b7d7c7bff8ff73d89d
cezarerbach <cezarerbach@gmail.com>
2025-12-26 12:56:01 -0300

    Update gera-boleto.js

--- api/gera-boleto.js
+++ api/gera-boleto.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
  //   const apiKey = req.headers['x-base44-api-key'];
   //  if (!apiKey || apiKey !== process.env.BASE44_INTERMEDIARY_KEY) {
    //     return res.status(401).json({ error: 'Unauthorized' });
-    }
+  //  }
 
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method not allowed' });
